c858a2ba94b2ca2dce1bdcd232cd48abef2a461f
Hendrik Sollich <hendrik@hoodie.de>
2019-02-13 23:06:55 +0100

    adding json to js

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 tiny set of tiny json tools for my own use, nothing fance here (yet)
 
 1. `shrink-json` just reserializes a json file but without a lot of the air in it
+2. `json-to-js` same as shrink but produces a js/ts file exporting the json as a string
---
+++ src/bin/json-to-js.rs
@@ -0,0 +1,22 @@
+use serde_json;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::env;
+
+fn main() -> Result<(), Box<dyn Error>>{
+
+    let content: Option<serde_json::Value> = env::args()
+        .nth(1)
+        .and_then(|p| File::open(p).ok())
+        .map(BufReader::new)
+        .map(|f| serde_json::from_reader(f))
+        .and_then(Result::ok);
+
+    let reserialized = serde_json::to_string(&content)?;
+
+
+    println!("export const data = '{}';", reserialized);
+
+    Ok(())
+}
